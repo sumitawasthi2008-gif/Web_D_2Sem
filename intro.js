4bd1a320f52ace3c5f4078354e1ea309d1ed545b
Sumit Awasthi <sumitawasthi2008@gmail.com>
2026-02-25 00:14:04 +0530

    Initial commit

---
+++ intro.js
@@ -0,0 +1,23 @@
+console.log("hello");
+document.write("Hello");
+var num =10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+var person={
+    name:'sumit awasthi',
+    age:'18',
+    title:'student'
+}
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
