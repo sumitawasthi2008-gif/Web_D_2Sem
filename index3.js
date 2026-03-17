77dfe81c2bfbadd911552a8467405eb2db2ef7e8
Sumit Awasthi <sumitawasthi2008@gmail.com>
2026-03-17 14:22:05 +0530

    experiment-6

---
+++ index3.js
@@ -0,0 +1,28 @@
+let colors = ["lightblue", "lightgreen", "lightyellow", "lavender", "lightpink"];
+let index = 0;
+
+function changeColor() {
+    document.body.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+}
+
+function changeHeading() {
+    let input = document.getElementById("inputText").value;
+    document.getElementById("heading").innerText = input;
+}
+
+function increaseFont() {
+    let h1 = document.getElementById("heading");
+    let size = window.getComputedStyle(h1).fontSize;
+    size = parseInt(size);
+    h1.style.fontSize = (size + 5) + "px";
+}
+
+function togglePara() {
+    let para = document.getElementById("para");
+    if (para.style.display === "none") {
+        para.style.display = "block";
+    } else {
+        para.style.display = "none";
+    }
+}
